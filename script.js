60ce658e3548eb00be09719173756e805219348c
Edualsena <eduedualsena12390@gmail.com>
2026-04-16 20:38:33 -0300

    projeto finalizado 0.1

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-fetch('https://pokeapi.co/api/v2/pokemon?offset=10&limit=10')
+function pegaPokemons(quantidade){
+    fetch('https://pokeapi.co/api/v2/pokemon?offset=10&limit='+quantidade)
     .then(response => response.json())
-    .then(allpokemon => console.log(allpokemon))
+    .then(allpokemon => {
+       
+        var pokemons = [];
+
+        allpokemon.results.map((val)=>{
+            
+
+            fetch(val.url)
+            .then(response => response.json())
+            .then(pokemonSingle => { 
+                pokemons.push({nome:val.name,imagem:pokemonSingle.sprites.front_default});
+
+                if(pokemons.length == quantidade){
+                    // finalizamos nossas requisições.
+
+                    var pokemonBoxes = document.querySelector('.pokemon-boxes');
+                    pokemonBoxes.innerHTML = "";
+                    
+                    //console.log(pokemons);
+                    pokemons.map((val)=>{
+                        pokemonBoxes.innerHTML+=`
+                        <div class="pokemon-box">
+                        <img src="`+val.imagem+`">
+                        <p>`+val.nome+`</p>
+                        </div>`;
+                       
+                    })
+                }
+            })
+
+
+        })
+
+        pokemons.map((val)=>{
+            console.log(val.nome);
+        })
+    })
+}
